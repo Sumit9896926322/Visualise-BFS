a04dbd0b128e7e7aa1a2afcb6aac4da6201ca03f
unknown <writeonsumit.com>
2020-11-19 00:55:14 +0800

    bfs working

--- bfs.js
+++ bfs.js
@@ -9,6 +9,7 @@ let cols;
 let source = null, destination = null;
 let sRow = -1, sCol = -1;
 let dRow = -1, dCol = -1;
+let result = false;
 
 const gridGenerator = (gridType) => {
     let nRows = 0;
@@ -47,7 +48,8 @@ const gridGenerator = (gridType) => {
         let innerContent = `<div class = "row row-${i}">`;
         for (let j = 0; j < nCols; j++) {
 
-            innerContent += `<div class="col col-${i}-${j}" row=${i} col = ${j} style="height: ${height}px;width:${width}px;border: 2px solid  #0a0349;"></div>`;
+            innerContent += `<div class="col col-${i}-${j}" row=${i} col = ${j} style="height: ${height}px;width:${width}px;border: 2px solid  #b7a329; 
+            "></div>`;
         }
         innerContent += `</div>`;
         content += innerContent;
@@ -72,13 +74,22 @@ const activateColumns = () => {
                 board[sRow][sCol] = 1;
 
             }
-            else {
-                destination = cols[i];
-                cols[i].style.backgroundColor = "red";
+            else if (!destination) {
                 dRow = cols[i].getAttribute("row");
                 dCol = cols[i].getAttribute("col");
+                if (dRow == sRow && dCol == sCol) {
+                    alert("Source and destination can't be same");
+                    return;
+                }
+                destination = cols[i];
+                cols[i].style.backgroundColor = "red";
                 board[dRow][dCol] = 2;
             }
+            // } else {
+            //     cols[i].style.backgroundColor = "black";
+            //     board[cols[i].getAttribute("row")][cols[i].getAttribute("col")] = -1;
+            //     console.log(board);
+            // }
 
         });
     }
@@ -86,6 +97,7 @@ const activateColumns = () => {
 
 }
 const createVisitedArray = () => {
+    console.log(board);
     let boardRow = board.length;
     let boardCol = board[0].length;
 
@@ -93,8 +105,9 @@ const createVisitedArray = () => {
     for (let i = 0; i < vis.length; i++)
         vis[i] = new Array(boardCol);
 
-    for (let i = 0; i < vis.length; i++)
-        vis[i].fill(false);
+    for (let j = 0; j < vis.length; j++)
+        vis[j].fill(false);
+
     return vis;
 }
 class Node {
@@ -109,14 +122,14 @@ const validNode = (vis, node) => {
     if (node.row < 0 || node.row >= vis.length || node.col < 0 || node.col >= vis[0].length) {
         return false;
     }
-    console.log(node);
+
     if (vis[node.row][node.col])
         return false;
 
     return true;
 }
 
-const bfs = () => {
+const bfs = async () => {
     let queue = [];
     let vis = createVisitedArray();
     vis[sRow][sCol] = true;
@@ -125,67 +138,95 @@ const bfs = () => {
 
     while (queue.length > 0) {
         let curr = queue.shift();
-        console.log(curr);
         if (curr.row == dRow && curr.col == dCol) {
             //reaches destination
             return;
         }
-        let rightNode = new Node(curr.row, parseInt(curr.col) + 1);
-        let topNode = new Node(parseInt(curr.row) + 1, curr.col);
+
+        let rightNode = new Node(parseInt(curr.row), parseInt(curr.col) + 1);
+
+        let topNode = new Node(parseInt(curr.row) + 1, parseInt(curr.col));
+
         let leftNode = new Node(parseInt(curr.row), parseInt(curr.col) - 1);
         let bottomNode = new Node(parseInt(curr.row) - 1, parseInt(curr.col));
+
         if (validNode(vis, rightNode)) {
-            fillColor(rightNode.row, rightNode.col);
-            vis[rightNode.row][rightNode.col] = true;
-            queue.push(rightNode);
-        }
-        if (validNode(vis, topNode)) {
-            fillColor(topNode.row, topNode.col);
-            vis[topNode.row][topNode.col] = true;
-            queue.push(topNode);
-        }
-        if (validNode(vis, leftNode)) {
-            fillColor(leftNode.row, leftNode.col);
-            vis[leftNode.row][leftNode.col] = true;
-            queue.push(leftNode);
-        }
-        if (validNode(vis, bottomNode)) {
-            fillColor(bottomNode.row, bottomNode.col);
-            vis[bottomNode.row][bottomNode.col] = true;
-            queue.push(bottomNode);
+            await fillColor(rightNode.row, rightNode.col).then(() => {
+                vis[rightNode.row][rightNode.col] = true;
+                queue.push(rightNode);
+            });
+
         }
 
+        if (validNode(vis, topNode)) {
+            await fillColor(topNode.row, topNode.col).then(() => {
+
+                vis[topNode.row][topNode.col] = true;
+                queue.push(topNode);
+            });
+
+        }
+
+        if (validNode(vis, leftNode)) {
+            await fillColor(leftNode.row, leftNode.col).then(() => {
+
+                vis[leftNode.row][leftNode.col] = true;
+                queue.push(leftNode);
+            });
+
+        }
+        if (validNode(vis, bottomNode)) {
+            await fillColor(bottomNode.row, bottomNode.col).then(() => {
+
+                vis[bottomNode.row][bottomNode.col] = true;
+                queue.push(bottomNode);
+            });
+
+        }
     }
 
 }
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+const fillColor = async (r, c) => {
+    await sleep(10);
 
-const fillColor = (r, c) => {
-    console.log(r, c);
     for (let i = 0; i < cols.length; i++) {
+        if (cols[i].getAttribute("row") == dRow && cols[i].getAttribute("col") == dCol) {
+            return;
+        }
         if (cols[i].getAttribute("row") == r && cols[i].getAttribute("col") == c) {
-            cols[i].style.backgroundColor = "#b7a329";
+            const elem = document.querySelector(`.col-${cols[i].getAttribute("row")}-${cols[i].getAttribute("col")}`);
+
+            cols[i].style.backgroundColor = "#0a0349";
 
         }
     }
 }
 
 smallGrid.addEventListener("click", () => {
+    source = null, destination = null;
     gridGenerator("small");
 });
 
 
 mediumGrid.addEventListener("click", () => {
+    source = null, destination = null;
     gridGenerator("medium");
 });
 
 largeGrid.addEventListener("click", () => {
+    source = null, destination = null;
     gridGenerator("large");
 });
 
 reset.addEventListener("click", () => {
+    source = null, destination = null;
     grid.innerHTML = "";
 });
 start.addEventListener("click", () => {
+
     if (!source || !destination) {
         alert("Choose source and destination carefully");
     } else {
